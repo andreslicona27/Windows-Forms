458cbc010521aa26de9a8e6a32782154d93213ec
Andres Licona <andreslicona27@icloud.com>
2022-11-15 13:08:01 +0100

    E5 advances

--- Ejercicios_Tema_4/Ejercicio_5/Ejercicio_5/Form1.Designer.cs
+++ Ejercicios_Tema_4/Ejercicio_5/Ejercicio_5/Form1.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbOne = new System.Windows.Forms.ListBox();
+            this.lbTwo = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnTransferTwo = new System.Windows.Forms.Button();
+            this.btnTransferOne = new System.Windows.Forms.Button();
+            this.lblNumElementos = new System.Windows.Forms.Label();
+            this.lblIndice = new System.Windows.Forms.Label();
+            this.tbElement = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbOne
+            // 
+            this.lbOne.FormattingEnabled = true;
+            this.lbOne.ItemHeight = 15;
+            this.lbOne.Location = new System.Drawing.Point(190, 33);
+            this.lbOne.Name = "lbOne";
+            this.lbOne.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbOne.Size = new System.Drawing.Size(120, 199);
+            this.lbOne.TabIndex = 0;
+            // 
+            // lbTwo
+            // 
+            this.lbTwo.FormattingEnabled = true;
+            this.lbTwo.ItemHeight = 15;
+            this.lbTwo.Location = new System.Drawing.Point(327, 33);
+            this.lbTwo.Name = "lbTwo";
+            this.lbTwo.Size = new System.Drawing.Size(120, 199);
+            this.lbTwo.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(23, 119);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(23, 148);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(138, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnTransferTwo
+            // 
+            this.btnTransferTwo.Location = new System.Drawing.Point(23, 206);
+            this.btnTransferTwo.Name = "btnTransferTwo";
+            this.btnTransferTwo.Size = new System.Drawing.Size(138, 23);
+            this.btnTransferTwo.TabIndex = 5;
+            this.btnTransferTwo.Text = "Transfer List Two";
+            this.btnTransferTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnTransferOne
+            // 
+            this.btnTransferOne.Location = new System.Drawing.Point(23, 177);
+            this.btnTransferOne.Name = "btnTransferOne";
+            this.btnTransferOne.Size = new System.Drawing.Size(138, 23);
+            this.btnTransferOne.TabIndex = 4;
+            this.btnTransferOne.Text = "Transfer List One";
+            this.btnTransferOne.UseVisualStyleBackColor = true;
+            // 
+            // lblNumElementos
+            // 
+            this.lblNumElementos.AutoSize = true;
+            this.lblNumElementos.Location = new System.Drawing.Point(23, 33);
+            this.lblNumElementos.Name = "lblNumElementos";
+            this.lblNumElementos.Size = new System.Drawing.Size(79, 15);
+            this.lblNumElementos.TabIndex = 6;
+            this.lblNumElementos.Text = "Elts in list one";
+            // 
+            // lblIndice
+            // 
+            this.lblIndice.AutoSize = true;
+            this.lblIndice.Location = new System.Drawing.Point(23, 62);
+            this.lblIndice.Name = "lblIndice";
+            this.lblIndice.Size = new System.Drawing.Size(96, 15);
+            this.lblIndice.TabIndex = 7;
+            this.lblIndice.Text = "Index of element";
+            // 
+            // tbElement
+            // 
+            this.tbElement.Location = new System.Drawing.Point(23, 90);
+            this.tbElement.Name = "tbElement";
+            this.tbElement.Size = new System.Drawing.Size(138, 23);
+            this.tbElement.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "List One";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "List Two";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(475, 254);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbElement);
+            this.Controls.Add(this.lblIndice);
+            this.Controls.Add(this.lblNumElementos);
+            this.Controls.Add(this.btnTransferTwo);
+            this.Controls.Add(this.btnTransferOne);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbTwo);
+            this.Controls.Add(this.lbOne);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ListBox lbOne;
+        private ListBox lbTwo;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnTransferTwo;
+        private Button btnTransferOne;
+        private Label lblNumElementos;
+        private Label lblIndice;
+        private TextBox tbElement;
+        private Label label1;
+        private Label label2;
     }
 }
--- Ejercicios_Tema_4/Ejercicio_5/Ejercicio_5/Form1.cs
+++ Ejercicios_Tema_4/Ejercicio_5/Ejercicio_5/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace Ejercicio_5
 {
     public partial class Form1 : Form
@@ -5,6 +7,27 @@ namespace Ejercicio_5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bool varibleRepeated = false;
+            foreach (var item in lbOne.Items)
+            {
+                   if (item.ToString().Equals(tbElement.Text))
+                {
+                    varibleRepeated = true;
+                }
+            }
+            if (!varibleRepeated)
+            {
+                lbOne.Items.Add(tbElement.Text);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
