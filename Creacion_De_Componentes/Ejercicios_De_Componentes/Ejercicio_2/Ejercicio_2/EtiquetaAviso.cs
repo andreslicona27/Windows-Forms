1faec914cc27135a08735eb4b9384300d05eea3b
Andres Licona <andreslicona27@icloud.com>
2023-01-13 13:55:59 +0100

    E2 part one done

---
+++ Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio_2/Ejercicio_2/EtiquetaAviso.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Drawing.Drawing2D;
+
+namespace Ejercicio_2
+{
+    public enum eMarca
+    {
+        Nada,
+        Cruz,
+        Circulo,
+        ImagenMarca
+    }
+
+    public partial class EtiquetaAviso : Control
+    {
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int grosor = 0; //Grosor de las líneas de dibujo
+            int offsetX = 0; //Desplazamiento a la derecha del texto
+            int offsetY = 0; //Desplazamiento hacia abajo del texto
+                             // Altura de fuente, usada como referencia en varias partes
+            int h = this.Font.Height;
+            //Esta propiedad provoca mejoras en la apariencia o en la eficiencia
+            // a la hora de dibujar
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //Dependiendo del valor de la propiedad marca dibujamos una
+            //Cruz o un Círculo
+            if (Gradient)
+            {
+                LinearGradientBrush l = new LinearGradientBrush(
+                    new Point(0, 0),
+                    new Point(this.Width, 0),
+                    ginitial,
+                    gfinal);
+
+                Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+                g.FillRectangle(l, rect);
+                l.Dispose();
+
+            }
+            switch (Marca)
+            {
+                case eMarca.Circulo:
+                    grosor = 20;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor,
+                    h, h);
+                    offsetX = h + grosor;
+                    offsetY = grosor;
+                    break;
+                case eMarca.Cruz:
+                    grosor = 3;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    g.DrawLine(lapiz, grosor, grosor, h, h);
+                    g.DrawLine(lapiz, h, grosor, grosor, h);
+                    offsetX = h + grosor;
+                    offsetY = grosor / 2;
+                    //Es recomendable liberar recursos de dibujo pues se
+                    //pueden realizar muchos y cogen memoria
+                    lapiz.Dispose();
+                    break;
+                case eMarca.ImagenMarca:
+
+                    break;
+            }
+
+            //Finalmente pintamos el Texto; desplazado si fuera necesario
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
+            b.Dispose();
+
+
+
+
+        }
+
+        private eMarca marca = eMarca.Nada;
+        [Category("Appearance")]
+        [Description("Indica el tipo de marca que aparece junto al texto")]
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+            get
+            {
+                return marca;
+            }
+        }
+
+        private bool gradient = false;
+        [Category("Appearance")]
+        [Description("Indicates if the componente has a linear gradient")]
+        public bool Gradient
+        {
+            set
+            {
+                gradient = value;
+                this.Refresh();
+            }
+            get
+            {
+                return gradient;
+            }
+        }
+
+        private Color ginitial;
+        [Category("Appearance")]
+        [Description("Indicate the initial color of the linear gradient")]
+        public Color GInitial
+        {
+            set
+            {
+                ginitial = value;
+                this.Refresh();
+            }
+            get
+            {
+                return ginitial;
+            }
+        }
+
+        private Color gfinal;
+        [Category("Appearance")]
+        [Description("Indicate the final color of the linear gradient")]
+        public Color GFinal
+        {
+            set
+            {
+                gfinal = value;
+                this.Refresh();
+            }
+            get
+            {
+                return gfinal;
+            }
+        }
+
+        private Image imagenMarca;
+        [Category("Appearance")]
+        [Description("Image that would show in the mark of the label")]
+        public Image ImageMarca
+        {
+            set
+            {
+                imagenMarca = value;
+                this.Refresh();
+            }
+            get
+            {
+                return imagenMarca;
+            }
+        }
+
+
+    }
+}
---
+++ Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio_2/TestAPP/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TestAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.etiquetaAviso2 = new Ejercicio_2.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // etiquetaAviso2
+            // 
+            this.etiquetaAviso2.GFinal = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.etiquetaAviso2.GInitial = System.Drawing.Color.Red;
+            this.etiquetaAviso2.Gradient = true;
+            this.etiquetaAviso2.Location = new System.Drawing.Point(12, 12);
+            this.etiquetaAviso2.Marca = Ejercicio_2.eMarca.Circulo;
+            this.etiquetaAviso2.Name = "etiquetaAviso2";
+            this.etiquetaAviso2.Size = new System.Drawing.Size(131, 53);
+            this.etiquetaAviso2.TabIndex = 1;
+            this.etiquetaAviso2.Text = "etiquetaAviso2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ejercicio_2.EtiquetaAviso etiquetaAviso2;
+    }
+}
+
