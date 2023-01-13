c3578b5c905e482fa6d4d9542fc9f820c4832a1a
Andres Licona <andreslicona27@icloud.com>
2023-01-13 10:26:53 +0100

    Guide Practice Of paint

--- Creacion_De_Componentes/MyFirstComponent/FirstAppWithComponent/Form1.Designer.cs
+++ Creacion_De_Componentes/MyFirstComponent/FirstAppWithComponent/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.labelTextbox1 = new MyFirstComponent.LabelTextbox();
+            this.etiquetaAviso1 = new MyFirstComponent.EtiquetaAviso();
+            this.etiquetaAviso2 = new MyFirstComponent.EtiquetaAviso();
+            this.etiquetaAviso3 = new MyFirstComponent.EtiquetaAviso();
             this.SuspendLayout();
             // 
             // labelTextbox1
@@ -37,14 +40,46 @@
             this.labelTextbox1.Name = "labelTextbox1";
             this.labelTextbox1.Posicion = MyFirstComponent.ePosicion.IZQUIERDA;
             this.labelTextbox1.Separacion = 0;
-            this.labelTextbox1.Size = new System.Drawing.Size(342, 168);
+            this.labelTextbox1.Size = new System.Drawing.Size(342, 20);
             this.labelTextbox1.TabIndex = 0;
+            this.labelTextbox1.TextLbl = "label1";
+            this.labelTextbox1.TextTxt = "";
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.Location = new System.Drawing.Point(42, 133);
+            this.etiquetaAviso1.Marca = MyFirstComponent.eMarca.Cruz;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(65, 15);
+            this.etiquetaAviso1.TabIndex = 1;
+            this.etiquetaAviso1.Text = "Mensaje";
+            // 
+            // etiquetaAviso2
+            // 
+            this.etiquetaAviso2.Location = new System.Drawing.Point(61, 99);
+            this.etiquetaAviso2.Marca = MyFirstComponent.eMarca.Nada;
+            this.etiquetaAviso2.Name = "etiquetaAviso2";
+            this.etiquetaAviso2.Size = new System.Drawing.Size(46, 13);
+            this.etiquetaAviso2.TabIndex = 2;
+            this.etiquetaAviso2.Text = "Mensaje";
+            // 
+            // etiquetaAviso3
+            // 
+            this.etiquetaAviso3.Location = new System.Drawing.Point(12, 154);
+            this.etiquetaAviso3.Marca = MyFirstComponent.eMarca.Circulo;
+            this.etiquetaAviso3.Name = "etiquetaAviso3";
+            this.etiquetaAviso3.Size = new System.Drawing.Size(99, 53);
+            this.etiquetaAviso3.TabIndex = 3;
+            this.etiquetaAviso3.Text = "Mensaje";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(410, 219);
+            this.Controls.Add(this.etiquetaAviso3);
+            this.Controls.Add(this.etiquetaAviso2);
+            this.Controls.Add(this.etiquetaAviso1);
             this.Controls.Add(this.labelTextbox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +90,9 @@
         #endregion
 
         private MyFirstComponent.LabelTextbox labelTextbox1;
+        private MyFirstComponent.EtiquetaAviso etiquetaAviso1;
+        private MyFirstComponent.EtiquetaAviso etiquetaAviso2;
+        private MyFirstComponent.EtiquetaAviso etiquetaAviso3;
     }
 }
 
---
+++ Creacion_De_Componentes/MyFirstComponent/MyFirstComponent/EtiquetaAviso.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstComponent
+{
+
+    public enum eMarca
+    {
+        Nada,
+        Cruz,
+        Circulo
+    }
+
+    public partial class EtiquetaAviso : Control
+    {
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int grosor = 0; //Grosor de las líneas de dibujo
+            int offsetX = 0; //Desplazamiento a la derecha del texto
+            int offsetY = 0; //Desplazamiento hacia abajo del texto
+                             // Altura de fuente, usada como referencia en varias partes
+            int h = this.Font.Height;
+            //Esta propiedad provoca mejoras en la apariencia o en la eficiencia
+            // a la hora de dibujar
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //Dependiendo del valor de la propiedad marca dibujamos una
+            //Cruz o un Círculo
+            switch (Marca)
+            {
+                case eMarca.Circulo:
+                    grosor = 20;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor,
+                    h, h);
+                    offsetX = h + grosor;
+                    offsetY = grosor;
+                    break;
+                case eMarca.Cruz:
+                    grosor = 3;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    g.DrawLine(lapiz, grosor, grosor, h, h);
+                    g.DrawLine(lapiz, h, grosor, grosor, h);
+                    offsetX = h + grosor;
+                    offsetY = grosor / 2;
+                    //Es recomendable liberar recursos de dibujo pues se
+                    //pueden realizar muchos y cogen memoria
+                    lapiz.Dispose();
+                    break;
+            }
+            //Finalmente pintamos el Texto; desplazado si fuera necesario
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
+            b.Dispose();
+
+
+        }
+
+        private eMarca marca = eMarca.Nada;
+        [Category("Appearance")]
+        [Description("Indica el tipo de marca que aparece junto al texto")]
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+            get
+            {
+                return marca;
+            }
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.Refresh();
+        }
+
+    }
+}
