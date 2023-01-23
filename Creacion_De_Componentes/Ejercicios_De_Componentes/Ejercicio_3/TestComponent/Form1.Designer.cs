32ae6de975bd4ec3be2fe5047aa02936c05b3141
Andres Licona <andreslicona27@icloud.com>
2023-01-23 13:53:52 +0100

    E3 advances

--- Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio_3/TestComponent/Form1.Designer.cs
+++ Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio_3/TestComponent/Form1.Designer.cs
@@ -31,8 +31,6 @@
             this.player1 = new Ejercicio_3.Player();
             this.cbName = new System.Windows.Forms.ComboBox();
             this.lblTime = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblDirectory = new System.Windows.Forms.Label();
             this.btnOpen = new System.Windows.Forms.Button();
             this.pbImages = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbImages)).BeginInit();
@@ -40,7 +38,7 @@
             // 
             // player1
             // 
-            this.player1.Location = new System.Drawing.Point(296, 157);
+            this.player1.Location = new System.Drawing.Point(214, 158);
             this.player1.Minutes = -60;
             this.player1.Name = "player1";
             this.player1.Seconds = 0;
@@ -72,7 +70,7 @@
             "18",
             "19",
             "20"});
-            this.cbName.Location = new System.Drawing.Point(12, 130);
+            this.cbName.Location = new System.Drawing.Point(12, 94);
             this.cbName.Name = "cbName";
             this.cbName.Size = new System.Drawing.Size(121, 21);
             this.cbName.TabIndex = 1;
@@ -80,41 +78,25 @@
             // lblTime
             // 
             this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(9, 114);
+            this.lblTime.Location = new System.Drawing.Point(9, 78);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(103, 13);
             this.lblTime.TabIndex = 2;
             this.lblTime.Text = "Time for each image";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 34);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // lblDirectory
-            // 
-            this.lblDirectory.AutoSize = true;
-            this.lblDirectory.Location = new System.Drawing.Point(9, 18);
-            this.lblDirectory.Name = "lblDirectory";
-            this.lblDirectory.Size = new System.Drawing.Size(69, 13);
-            this.lblDirectory.TabIndex = 4;
-            this.lblDirectory.Text = "Add directory";
-            // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(12, 60);
+            this.btnOpen.Location = new System.Drawing.Point(12, 34);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.Size = new System.Drawing.Size(121, 23);
             this.btnOpen.TabIndex = 5;
-            this.btnOpen.Text = "Open";
+            this.btnOpen.Text = "Open Directory";
             this.btnOpen.UseVisualStyleBackColor = true;
             this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // pbImages
             // 
-            this.pbImages.Location = new System.Drawing.Point(296, 33);
+            this.pbImages.Location = new System.Drawing.Point(214, 34);
             this.pbImages.Name = "pbImages";
             this.pbImages.Size = new System.Drawing.Size(172, 118);
             this.pbImages.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -125,11 +107,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 236);
+            this.ClientSize = new System.Drawing.Size(410, 236);
             this.Controls.Add(this.pbImages);
             this.Controls.Add(this.btnOpen);
-            this.Controls.Add(this.lblDirectory);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lblTime);
             this.Controls.Add(this.cbName);
             this.Controls.Add(this.player1);
@@ -146,8 +126,6 @@
         private Ejercicio_3.Player player1;
         private System.Windows.Forms.ComboBox cbName;
         private System.Windows.Forms.Label lblTime;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label lblDirectory;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.PictureBox pbImages;
     }
--- Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio_3/TestComponent/Form1.cs
+++ Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio_3/TestComponent/Form1.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace TestComponent
 {
     public partial class Form1 : Form
     {
+        Image[] images;
+        int contImge = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,21 +27,48 @@ namespace TestComponent
 
         }
 
+
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            OpenFileDialog images = new OpenFileDialog();
-            images.Filter = "Directorios|*.this.directory";
+            DirectoryInfo directory;
+            FileInfo[] files;
+            string directoryPath = string.Empty;
 
-            try {
-                if (images.ShowDialog() == DialogResult.OK)
-                {
-                    string dir = images.FileName;
-                    // hacer algo con el directorio seleccionado
-                }
-            }
-            catch
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "png files (*.png)|*.png|jpg files(*.jpg)|*.jpg";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
 
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    directoryPath = openFileDialog.FileName;
+                }
+                directory = new DirectoryInfo(directoryPath);
+                files = directory.GetFiles().ToArray();
+
+            }
+
+            try
+            {
+                images = new Image[files.Length];
+                if (directoryPath != string.Empty)
+                {
+                    for (int i = 0; i < directoryPath.Length; i++)
+                    {
+                        images[i] = Image.FromFile(files[i].FullName);
+                    }
+                }
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("We dont find the directory", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("An error ocurred in the selection of the directory", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
 
