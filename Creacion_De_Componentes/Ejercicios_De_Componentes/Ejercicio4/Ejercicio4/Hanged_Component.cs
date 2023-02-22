9d7371115ca583a449188f37040c70cd82eac5db
Andres Licona <andreslicona27@icloud.com>
2023-02-22 21:30:57 +0100

    advances

--- Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio4/Ejercicio4/Hanged_Component.cs
+++ Creacion_De_Componentes/Ejercicios_De_Componentes/Ejercicio4/Ejercicio4/Hanged_Component.cs
@@ -12,6 +12,14 @@ namespace Ejercicio4
 {
     public partial class Hanged_Component: UserControl
     {
+        private int DEFAULT_WIDTH = 185;
+        private int DEFAULT_HEIGHT = 205;
+
+        private float DEFAULT_UNIT_SCALE_X = 1f;
+        private float DEFAULT_UNIT_SCALE_Y = 1f;
+
+        private float UNIT_SCALE_X;
+        private float UNIT_SCALE_Y;
         public Hanged_Component()
         {
             InitializeComponent();
@@ -43,9 +51,38 @@ namespace Ejercicio4
         protected void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
+            float thickness = scaleX(2);
+            float x = scaleX(0);
+            float y = scaleY(200);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Pen pen = new Pen(Color.Black, thickness);
+
+            switch (Mistakes)
+            {
+                case 0:
+                    break;
+                case 1:
+                    goto case 0;
+                case 2:
+                    goto case 1;
+                case 3:
+                    goto case 2;
+                case 4:
+                    goto case 3;
+                case 5:
+                    goto case 4;
+                case 6:
+                    goto case 5;
+                case 7:
+                    goto case 6;
+            }
 
         }
 
+        private float scaleX(float value) => value * UNIT_SCALE_X;
+
+        private float scaleY(float value) => value * UNIT_SCALE_Y;
 
         [Category("The Property Change")]
         [Description("Happens when the propertie mistakes changes")]
